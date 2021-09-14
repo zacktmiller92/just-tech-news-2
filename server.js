ac0dbb1960b3f1069bb504c2067d7d971e42085c
zacktmiller92 <zacktmiller92@gmail.com>
2021-09-13 22:01:41 -0400

    added homepage logic

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require('express');
-const routes = require('./routes');
+const routes = require('./controllers');
 const sequelize = require('./config/connection');
+const path = require('path');
+
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -8,8 +14,13 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// load static
+app.use(express.static(path.join(__dirname, 'public')));
 // turn on routes
 app.use(routes);
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
